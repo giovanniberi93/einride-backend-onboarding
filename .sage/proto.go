247c5a335efce4594b5d0308e43cb8ecc384e106
Giovanni Beri <giovanni.beri@einride.tech>
2025-05-15 14:36:20 +0200

    feat: add code generation

--- .sage/proto.go
+++ .sage/proto.go
@@ -4,14 +4,48 @@ import (
 	"context"
 
 	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
 	"go.einride.tech/sage/tools/sgbuf"
 )
 
 type Proto sg.Namespace
 
 func (Proto) Default(ctx context.Context) error {
-	sg.Deps(ctx)
+	sg.Deps(ctx, Proto.BufModFormat, Proto.BufModLint)
+	sg.Deps(ctx, Proto.BufGenerate)
 	return nil
+}
+
+func (Proto) BufGenerate(ctx context.Context) error {
+	sg.Deps(ctx, Proto.ProtocGenGo)
+	sg.Logger(ctx).Println("generating proto stubs...")
+	cmd := sgbuf.Command(ctx, "generate", "--output", sg.FromGitRoot(), "--template", "buf.gen.yaml", "--path", "einride")
+	cmd.Dir = sg.FromGitRoot("proto")
+	return cmd.Run()
+}
+
+func (Proto) ProtocGenGo(ctx context.Context) error {
+	sg.Logger(ctx).Println("installing...")
+	_, err := sgtool.GoInstallWithModfile(
+		ctx,
+		"google.golang.org/protobuf/cmd/protoc-gen-go",
+		sg.FromGitRoot("go.mod"),
+	)
+	return err
+}
+
+func (Proto) BufModFormat(ctx context.Context) error {
+	sg.Logger(ctx).Println("formatting proto file...")
+	cmd := sgbuf.Command(ctx, "format", "--write")
+	cmd.Dir = sg.FromGitRoot("proto")
+	return cmd.Run()
+}
+
+func (Proto) BufModLint(ctx context.Context) error {
+	sg.Logger(ctx).Println("linting proto file...")
+	cmd := sgbuf.Command(ctx, "lint")
+	cmd.Dir = sg.FromGitRoot("proto")
+	return cmd.Run()
 }
 
 func (Proto) BufModInit(ctx context.Context) error {
